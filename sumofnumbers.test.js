331581e2950646b42201d2955da01cfa80a55743
Philip Johnson <johnson@hawaii.edu>
2017-04-08 14:42:28 -1000

    Fix eslint error.

--- sumofnumbers.test.js
+++ sumofnumbers.test.js
@@ -1,11 +1,26 @@
 /* eslint-env mocha, chai */
-/* global sumFor, sumWhile, sumRecursion */
+/* global sumFor, sumWhile, sumRecursion, sumTheSimpleWay, chai */
 
 describe('SumOfNumbers', function () {
   const testNums = [1, 2, 3, 4];
   describe('sumFor()', function () {
     it('should return 10 for testnums', function () {
       chai.expect(sumFor(testNums)).to.equal(10);
+    });
+  });
+  describe('sumWhile()', function () {
+    it('should return 10 for testnums', function () {
+      chai.expect(sumWhile(testNums)).to.equal(10);
+    });
+  });
+  describe('sumRecursion()', function () {
+    it('should return 10 for testnums', function () {
+      chai.expect(sumRecursion(testNums)).to.equal(10);
+    });
+  });
+  describe('sumTheSimpleWay()', function () {
+    it('should return 10 for testnums', function () {
+      chai.expect(sumTheSimpleWay(testNums)).to.equal(10);
     });
   });
 });
